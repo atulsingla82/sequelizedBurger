d0b18c3864a5b6dc8f1897120cc7c3289f4263a3
atulsingla82 <Lucky@Atuls-MBP.fios-router.home>
2017-05-23 00:21:25 -0400

    fixing bugs

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,18 @@
 // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
 // *********************************************************************************
 
-var Sequelize = require("sequelize");
-
-// Creates mySQL connection using Sequelize
-if (process.env.JAWSDB_URL) {
-    connection = new Sequelize(process.env.JAWSDB_URL);
-} else {
-
-    sequelize = new Sequelize("Burgers2", "root", {
-        host: "localhost",
-        dialect: "mysql",
-        PORT: 8080
-    });
-};
-// Exports the connection for other files to use
-module.exports = sequelize;
+// var Sequelize = require("sequelize");
+//
+// // Creates mySQL connection using Sequelize
+// if (process.env.JAWSDB_URL) {
+//     connection = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//
+//     sequelize = new Sequelize("Burgers2", "root", {
+//         host: "localhost",
+//         dialect: "mysql",
+//         PORT: 8080
+//     });
+// };
+// // Exports the connection for other files to use
+// module.exports = sequelize;
